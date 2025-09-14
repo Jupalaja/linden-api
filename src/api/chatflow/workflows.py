5f8dbf97ea89262fab9e0328e6dc48fbd3c0a803
jupalaja <jupalaja@gmail.com>
2025-09-14 18:18:33 -0500

    :recycle: refactor state after invalid input

--- src/api/chatflow/workflows.py
+++ src/api/chatflow/workflows.py
@@ -417,7 +417,7 @@ async def validate_state_workflow(
             history_messages,
             model,
             PROMPT_INVALID_STATE,
-            ChatflowState.OFFER_BOOK_CALL,
+            ChatflowState.REQUEST_RESOLVED_AWAIT_NEW_MESSAGE,
             interaction_data,
         )
 
